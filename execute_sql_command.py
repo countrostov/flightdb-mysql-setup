5ab40d402f2e79481aac96c51b211f6dfdfd0f02
CountRostov <130492623+countrostov@users.noreply.github.com>
2023-10-23 00:25:47 +0530

    executes a given sql query

---
+++ execute_sql_command.py
@@ -0,0 +1,29 @@
+def execute_sql_command(cursor, database, query):
+
+    import mysql.connector
+
+
+    TABLES = query
+    # TABLES['customer'] = (
+    #     "CREATE TABLE `customer` ("
+    #     "  `phone_number` INT(255) NOT NULL,"
+    #     "  `first_name` varchar(14) NOT NULL,"
+    #     "  `last_name` varchar(16) NOT NULL,"
+    #     "  `birth_date` date NOT NULL,"
+    #     "  `gender` enum('M','F') NOT NULL,"
+    #     "  `house_number` SMALLINT(255) NOT NULL,"
+    #     "  `street` varchar(14) NOT NULL,"
+    #     "  `city` varchar(14) NOT NULL,"
+    #     "  `emergency_contact` INT(255) NOT NULL,"
+    #     "  PRIMARY KEY (`phone_number`)"
+    #     ") ")
+
+    for table_name in TABLES:
+        table_description = TABLES[table_name]
+        try:
+            print("Executing query",table_description)
+            cursor.execute(table_description)
+        except mysql.connector.Error as err:
+            print(err.msg)
+        print("SQL query executed successfully")
+
