b2f4da960716ba39667d7c3729e8f5fb27867954
CountRostov <130492623+countrostov@users.noreply.github.com>
2023-10-23 22:37:17 +0530

    copassgener list update script

---
+++ create_connector_booking_copassenger.py
@@ -0,0 +1,36 @@
+import mysql.connector
+from mysql.connector import Error
+from Connector_Close import *
+from Search_query import *
+from flight_booking_query import *
+from Create_Tables import *
+from execute_sql_command import *
+from insert_query import *
+
+try:
+    connection = mysql.connector.connect(host='localhost',
+                                         database='flightDB1',
+                                         user='root',
+                                         password='flightDB')
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("You're connected to database: ", record)
+
+except Error as e:
+    print("Error while connecting to MySQL", e)
+
+
+database = 'flightDB1'
+#bid = cursor.lastrowid
+query = ("INSERT INTO copassenger_list "
+        "(bid ,dep_id ) "
+        "VALUES (%s, %s)")
+insert_data = ('1', '2' )
+
+insert_query(cursor, connection , database,query, insert_data)
+
+connection_close(cursor, connection)
