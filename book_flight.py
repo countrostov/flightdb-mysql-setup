645e2f5ffd963bb51cb285a6dfa2915a89f5686b
CountRostov <130492623+countrostov@users.noreply.github.com>
2023-10-24 23:24:26 +0530

    api created for booking flight for customer

---
+++ book_flight.py
@@ -0,0 +1,12 @@
+def book_flight(cursor, connection , database, insert_data):
+    import mysql.connector
+    from mysql.connector import Error
+    from flight_booking_query import flight_booking_query
+
+    bid = cursor.lastrowid
+    query = ("INSERT INTO booking "
+            "(bid ,no_of_seats, customer, boarding_status,flightschedule_id ) "
+            "VALUES (%s, %s, %s, %s, %s)")
+    insert_data1 = (bid,) + insert_data
+    flight_booking_query(cursor, connection , database,query, insert_data1)
+
--- create_connector_booking.py
+++ create_connector_booking.py
@@ -1,6 +1,7 @@
 import mysql.connector
 from mysql.connector import Error
 from Connector_Close import *
+from book_flight import *
 from Search_query import *
 from flight_booking_query import *
 from Create_Tables import *
@@ -23,20 +24,9 @@ try:
 except Error as e:
     print("Error while connecting to MySQL", e)
 
-# #copassenger
-# dep_id = cursor.lastrowid
-# query = ("INSERT INTO copassenger "
-#          "(dep_id ,first_name,last_name, emergency_contact, cust) "
-#          "VALUES (%s, %s, %s, %s, %s)")
-# insert_data = (dep_id, 'san', 'tha', '1100000011', '1000000001')
-
 database = 'flightDB1'
-bid = cursor.lastrowid
-query = ("INSERT INTO booking "
-        "(bid ,no_of_seats, customer, boarding_status,flightschedule_id ) "
-        "VALUES (%s, %s, %s, %s, %s)")
-insert_data = (bid, '2', '1000000002','N','3')
 
-flight_booking_query(cursor, connection , database,query, insert_data)
+insert_data = ('2', '1000000002','N','5')
+book_flight(cursor, connection , database,insert_data)
 
 connection_close(cursor, connection)
